837886aa354c2c8907f8b18d33dfa569be42d709
Angel Samano Hamud <ansahamano@gmail.com>
2026-05-11 20:07:12 +0200

    Refactor CSS and Tailwind configuration; update color variables and font families for consistency

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,31 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      colors: {
+        neutral: {
+          0: 'hsl(var(--color-neutral-0) / <alpha-value>)',
+          200: 'hsl(var(--color-neutral-200) / <alpha-value>)',
+          300: 'hsl(var(--color-neutral-300) / <alpha-value>)',
+          600: 'hsl(var(--color-neutral-600) / <alpha-value>)',
+          700: 'hsl(var(--color-neutral-700) / <alpha-value>)',
+          800: 'hsl(var(--color-neutral-800) / <alpha-value>)',
+          900: 'hsl(var(--color-neutral-900) / <alpha-value>)',
+        },
+        orange: {
+          500: 'hsl(var(--color-orange-500) / <alpha-value>)',
+        },
+        blue: {
+          500: 'hsl(var(--color-blue-500) / <alpha-value>)',
+          700: 'hsl(var(--color-blue-700) / <alpha-value>)',
+        },
+      },
+      screens: {
+        mobile: '375px',
+        desktop: '1440px',
+      },
       fontFamily: {
-        sans: ['Bricolage Grotesque', 'ui-sans-serif', 'system-ui'],
+        sans: ['DM Sans', 'ui-sans-serif', 'system-ui'],
+        display: ['Bricolage Grotesque', 'ui-sans-serif', 'system-ui'],
       },
     },
   },
